19b6b1e22da75b9da2bffbf70c77b18fd807a1d3
JunPark <phjun0324@gmail.com>
2022-01-11 23:44:27 +0900

    새브랜치 만들기전에 저장

--- static/detail.js
+++ static/detail.js
@@ -3,3 +3,29 @@ function sign_out() {//로그아웃 버튼을 누르면
     alert('로그아웃!')
     window.location.href = "/login"
 }
+
+function showReview() {
+                $.ajax({
+                    type: "GET",
+                    url: "/review",
+                    data: {},
+                    success: function (response) {
+                        let reviews = response['all_reviews']
+                        for (let i = 0; i<reviews.length; i++ ) {
+                            let title = reviews[i]['title']
+                            let author = reviews[i]['author']
+                            let review = reviews[i]['review']
+
+                            let temp_html = `
+                                            <tr>
+                                                <td>${title}</td>
+                                                <td>${author}</td>
+                                                <td>${review}</td>
+                                            </tr>
+                                            `
+                            $('#reviews-box').append(temp_html)
+
+                        }
+                    }
+                })
+            }
