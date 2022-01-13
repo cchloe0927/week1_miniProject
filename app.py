0d010839fb9348e64f9742a6ee51bb2be0b52337
DESKTOP-CP8SFTR\ljj52 <ljj5256@gmail.com>
2022-01-13 15:27:58 +0900

    1

--- app.py
+++ app.py
@@ -13,67 +13,73 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
+# DB 접속
 client = MongoClient('localhost', 27017)
 db = client.week1Project
 
 
-#####메인페이지 토큰을 가져와서 해당유저데이터 불러오기('/')######
+##### 메인페이지 토큰을 가져와서 해당유저데이터 불러오기('/')######
 @app.route('/')
 def home():
+    # 클라이언트로 부터 토큰이 담긴 쿠키를 받는다.
     token_receive = request.cookies.get('mytoken')
     try:
+        # payload 생성
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 사용자의 정보를 user_info 에 담는다.
         user_info = db.users.find_one({"username": payload["id"]})
+        # index.html 로 user_info 를 넘긴다.
         return render_template('index.html', user_info=user_info)
 
+    # 토큰이 만료되었거나 정보가 없는 경우, 로그인 페이지로  이동
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login"))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login"))
 
 
-#####로그인하면 메세지 띄우기(/login)######
+##### 로그인하면 메세지 띄우기(/login)######
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
 
-#####유저네임을 통해서 프로필 페이지 보여주기(/user)######
+##### 유저네임을 통해서 프로필 페이지 보여주기(/user)######
 @app.route('/user/<username>')
 def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+        # 내 프로필이면 True, 다른 사람의 프로필 페이지면 False 의 status 값을 가진다.
+        status = (username == payload["id"])
 
+        # 사용자의 정보를 db 에서 가져와 user_info 에 담는다.
         user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)  # status를 이용해서 프로필 수정 보이기/숨기기
+        # user.html 로 user_info 와 status 값을 넘긴다.
+        return render_template('user.html', user_info=user_info, status=status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
 
-#####유저네임과 pw 데이터 받고 payload, 토큰 넘겨주기(/login)######
+##### 유저네임과 pw 데이터 받고 payload, 토큰 넘겨주기(/login)######
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    # 로그인
-    # id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
+    # id, pw를 받아서 맞춰보고, 토큰을 만들어 발급한다.
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
-    # bcrypt로 비밀번호를 해쉬화 한다
+    # bcrypt 로 비밀번호를 해쉬화 한다
     hash_pw = bcrypt.hashpw(password_receive.encode('utf-8'), bcrypt.gensalt())
     hashed_pw = hash_pw.decode('utf-8')
     check_pw_match = bcrypt.checkpw(password_receive.encode('utf-8'), hash_pw)
-    # print(check_pw_match)
     current_user = db.users.find_one({'username': username_receive})
-    # print(current_user)
-    # pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    # hash화 된
     if current_user and check_pw_match:
         payload = {
             'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         print(token)
@@ -147,7 +153,7 @@ def save_img():
         return redirect(url_for("home"))
 
 
-# 게시물 포스팅
+#####프로필페이지에서 프로필 사진업로드(/user)######
 @app.route('/posting', methods=['POST'])
 def posting():
     token_receive = request.cookies.get('mytoken')
@@ -188,12 +194,13 @@ def posting():
         return redirect(url_for("home"))
 
 
-# 전체게시물 보여주기
+# 게시물 불러오기
 @app.route('/listing', methods=['GET'])
 def listing():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 클라이언트에서 넘어온 username 을 username_receive 에 받는다.
         username_receive = request.args.get("username_give")
         if username_receive == "":
             posts = list(db.posts.find({}).sort("date", -1))
@@ -210,89 +217,51 @@ def listing():
         return redirect(url_for("home"))
 
 
+# 좋아요 기능
 @app.route('/update_like', methods=['POST'])
 def update_like():
     token_receive = request.cookies.get('mytoken')
     try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])  # 토큰 가져와서
-        user_info = db.users.find_one({"username": payload["id"]})  # 누가 좋아요를 눌렀는지 확인하고
-        post_id_receive = request.form["post_id_give"]  # 포스트의 오브젝트를 확인하고
-        type_receive = request.form["type_give"]  # 어떤 종류의 좋아요인지
-        action_receive = request.form["action_give"]  # 실행하는건지 취소하는건지 액션확인해서
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # 사용자를 확인
+        user_info = db.users.find_one({"username": payload["id"]})
+        # 게시물을 확인
+        post_id_receive = request.form["post_id_give"]
+        # 좋아요의 종류 확인
+        type_receive = request.form["type_give"]
+        # 실행인지 취소인지 확인
+        action_receive = request.form["action_give"]
+
+        # db 에 저장될 형식
         doc = {
             "post_id": post_id_receive,
             "username": user_info["username"],
             "type": type_receive,
         }
-        if action_receive == "like":  # 실행하는거면 저장
+        # 좋아요가 실행이면
+        if action_receive == "like":
+            # 저장한다.
             db.likes.insert_one(doc)
-        else:  # 아니면 삭제
+        else:  # 아니면 삭제한다.
             db.likes.delete_one(doc)
-        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})  # 동작 완료 후 좋아요 개수 확인해서
-        return jsonify({"result": "success", "count": count})  # 클라이언트로 넘겨줌
+        # db 에서 해당 게시물의 좋아요 개수를 확인해서 count 에 담는다.
+        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
 
-
-
+        # count 와 결과값을 클라이언트로 넘겨준다.
+        return jsonify({"result": "success", "count": count})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
-# @app.route('/detail_likes', methods=['GET'])
-# def get_likes():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         num_receive = request.args.get("num_give")
-#         posts = db.posts.find_one({"num": num_receive})
-#         for post in posts:
-#             post["_id"] = str(post["_id"])
-#             post["count_heart"] = db.likes.count_documents(
-#             {"post_id": post["_id"], "type": "heart"})  # 해당 글의 like 갯수를 파악
-#             post["heart_by_me"] = bool(db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": payload[
-#             'id']}))  # jwt토큰을 확인해서 username을 꺼내고 like타입을 확인해서 해당 게시글에 내 정보가 있으면 내가 좋아요를 눌렀는지 알게 됨
-#         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts": posts})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
-# 게시물 상세페이지 보여주기
-
+# num 에 해당하는 게시물의 상세페이지로 이동
 @app.route('/detail/<num>')
 def detail(num):
     try:
+        # db 에서 해당 num 을 가진 게시물의 정보를 search_post 에 담는다.
         search_post = db.posts.find_one({'num': num})
-
+        # search_post 를 detail.html 로 넘긴다.
         return render_template("detail.html", search_post=search_post)
     except ():
         return redirect(url_for("index"))
-
-@app.route('/detail', methods=['POST'])
-def detail():
-    posts_receive = request.form['posts_give']
-    print(posts_receive)
-    search_post = list(db.posts.find({}))
-    print(search_post)
-    for post in search_post:
-        post["_id"] = str(post["_id"])
-        if posts_receive == post['_id']:
-            username = post['username']
-            place_pic = post['place_pic']
-            contents = post['contents']
-
-            pass_pic = {'username': username, 'place_pic': place_pic, 'contents': contents}
-            print(pass_pic)
-            return render_template("detail.html", pass_pic=pass_pic)
-
-@app.route('/detail/<num>')
-def detail(num):
-    try:
-        search_post = db.posts.find_one({'num': num})
-
-        return render_template("detail.html", search_post=search_post)
-    except ():
-        return redirect(url_for("index"))
-
-
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/myjs.js
+++ static/myjs.js
@@ -1,26 +1,31 @@
+//username에 반응하여 게시물을 불러온다.
 function listing(username) {
+    //만약 username에 담긴 값이 없을 경우 username은 없는 것으로 간주한다.
     if (username == undefined) {
         username = ""
     }
+    //서버에서 정보 받아오기 전에 게시물이 담길 공간을 비운다.
     $("#post-box").empty()
     $.ajax({
         type: "GET",
+        //서버에 사용자의 username을 보낸다.
         url: `/listing?username_give=${username}`,
         data: {},
         success: function (response) {
             if (response["result"] == "success") {
                 let posts = response['posts']
                 for (let i = 0; i < posts.length; i++) {
+                    //각 post 에서 필요한 값을 가져온다.
                     let post = posts[i]
                     let num = post['num']
                     let place_pic = post['place_pic']
                     let profile_name = post['profile_name']
-
                     let time_post = new Date(post["date"])
                     let time_before = time2str(time_post)
-
-                    let class_heart = post['heart_by_me'] ? "fa-heart" : "fa-heart-o" //class_heart는 첫번쨰항이 true면 fa-heart이고 false면 fa-heart-o이다
+                     //class_heart는 첫번쨰항이 true면 fa-heart이고 false면 fa-heart-o이다.
+                    let class_heart = post['heart_by_me'] ? "fa-heart" : "fa-heart-o"
                     let count_heart = post['count_heart']
+                    // 가져온 값들을 원하는 곳에 넣어준다.
                     let temp_html = `<div style="margin:10px 0px 10px 20px; width: 18rem;" class="card" id="${post["_id"]}">
                                                 <div class="pic" >
                                                     <img style="height: 13rem; object-fit: cover"  src="../static/place_pic/${place_pic}" id="${post["_id"]}" onclick="location.href = '/detail/${num}'">
@@ -32,6 +37,7 @@ function listing(username) {
                                                     </nav>
                                                 </div>
                                          </div>`
+                    //게시물이 담길 공간으로 옮긴다.
                     $('#post-box').append(temp_html)
                 }
             }
@@ -39,42 +45,51 @@ function listing(username) {
     });
 }
 
-
+//좋아요 개수를 나타내는 숫자의 형식을 정해준다.
 function num2str(count) {
+    //만개가 넘으면 정수 뒤에 K
     if (count > 10000) {
         return parseInt(count / 1000) + "k"
     }
+    // 500개가 넘으면 소수점 뒤에 K
     if (count > 500) {
         return parseInt(count / 100) / 10 + "k"
     }
+    // 0개일 때는 숫자가 없다.
     if (count == 0) {
         return ""
-    }
+    }//나머지는 그대로 나타낸다.
     return count
 }
 
+//게시물이 작성된지 얼마나 되었는가를 보여준다.
 function time2str(date) {
     let today = new Date()
     let time = (today - date) / 1000 / 60  // 분
 
+    //60초가 넘어가는 경우에는 분으로 보여준다.
     if (time < 60) {
         return parseInt(time) + "분 전"
     }
+    //60분이 넘어가는 경우에는 시간으로 보여준다.
     time = time / 60  // 시간
     if (time < 24) {
         return parseInt(time) + "시간 전"
     }
+    //24시간이 넘어가는 경우에는 일 수로 보여준다.
     time = time / 24
     if (time < 7) {
         return parseInt(time) + "일 전"
     }
+    //7일 이상일 때에는 날짜로 보여준다.
     return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
 }
-
+//게시물의 post_id와 좋아요의 type을 확인해서 좋아요기능을 구현한다.
 function toggle_like(post_id, type) {
     console.log(post_id, type)
     let $a_like = $(`#${post_id} a[aria-label='heart']`)
     let $i_like = $a_like.find("i")
+    //좋아요가 되어있는거면 좋아요를 취소한다.
     if ($i_like.hasClass("fa-heart")) {
         $.ajax({
             type: "POST",
@@ -86,10 +101,12 @@ function toggle_like(post_id, type) {
             },
             success: function (response) {
                 console.log("unlike")
+                //찬 하트를 숨기고 빈 하트를 보여준다.
                 $i_like.addClass("fa-heart-o").removeClass("fa-heart")
                 $a_like.find("span.like-num").text(response["count"])
             }
         })
+        //아니면 좋아요를 실행한다.
     } else {
         $.ajax({
             type: "POST",
@@ -101,6 +118,7 @@ function toggle_like(post_id, type) {
             },
             success: function (response) {
                 console.log("like")
+                //빈 하트를 숨기고 찬 하트를 보여준다.
                 $i_like.addClass("fa-heart").removeClass("fa-heart-o")
                 $a_like.find("span.like-num").text(response["count"])
             }
